4347392493fc490ceb5bc3cb0c5ccfa3e5ef398f
Tyler <tylerthomasallen@gmail.com>
2018-10-05 09:38:09 -0700

    Connect rails backend to react frontend

---
+++ BenchBnB/frontend/entry.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Wow, full stack project running!</h1>, root);
+});
