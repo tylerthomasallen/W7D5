f3630be67f46abaaf41a6bfbe89d835ae695fe41
Tyler <tylerthomasallen@gmail.com>
2018-10-08 14:23:22 -0700

    Add google maps api and render map

--- BenchBnB/frontend/components/bench/bench_map.jsx
+++ BenchBnB/frontend/components/bench/bench_map.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class BenchMap extends React.Component {
+
+  componentDidMount() {
+    const map = ReactDOM.findDOMNode(this.refs.map);
+
+    const mapOptions = {
+      center: { lat: 37.7758, lng: -122.435 }, // this is SF
+      zoom: 13
+    };
+
+    this.map = new google.maps.Map(map, mapOptions);
+  }
 
 
   render() {
     return (
-      <div id='map-container' ref='map'>
+      <div id='map' ref='map'>
 
       </div>
 
@@ -14,3 +26,28 @@ class BenchMap extends React.Component {
 }
 
 export default BenchMap;
+
+// class BenchMap extends React.Component {
+//   //...
+//
+//   componentDidMount() {
+//     // set the map to show SF
+//     const mapOptions = {
+//       center: { lat: 37.7758, lng: -122.435 }, // this is SF
+//       zoom: 13
+//     };
+//
+//     // wrap this.mapNode in a Google Map
+//     this.map = new google.maps.Map(this.mapNode, mapOptions);
+//   }
+//
+//   render() {
+//     return (
+//       // ...
+//       <div ref={ map => this.mapNode = map }> // this ref gives us access to the map dom node
+//       // ...
+//     )
+//   }
+//
+//   //...
+// }
